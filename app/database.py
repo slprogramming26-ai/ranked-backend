6b084286fd9685ad7ebd82447f353cbb596136cc
slprogramming26-ai <sl-programming26@gmail.com>
2026-04-17 12:56:10 +0200

    fix db pooler connection

--- app/database.py
+++ app/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-import psycopg2
-
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_passwort}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
