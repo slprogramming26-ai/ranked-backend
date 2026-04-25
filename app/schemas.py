02bd2cf2427fba59223d946f7203903395573e0a
slprogramming26-ai <sl-programming26@gmail.com>
2026-04-25 18:52:49 +0200

    change image_url to profile_pic

--- app/schemas.py
+++ app/schemas.py
@@ -46,7 +46,7 @@ class UserCreate(BaseModel):
 class UserDetails(BaseModel):
     vibe_factor_1: str
     vibe_factor_2: str
-    image_url: Optional[str] = None
+    profile_picture_url: Optional[str] = None 
     biography: str
 
 class GetUserOut(BaseModel):
@@ -54,7 +54,7 @@ class GetUserOut(BaseModel):
     username: str
     vibe_factor_1: Optional[str] = None 
     vibe_factor_2: Optional[str] = None
-    image_url: Optional[str] = None
+    profile_picture_url: Optional[str] = None 
     biography: str
 
     model_config = ConfigDict(from_attributes=True)
